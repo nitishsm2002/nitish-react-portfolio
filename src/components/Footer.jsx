a317e421ed98c88c9511db7c5fd99a5969c60807
nitishsm2002 <nitishdhavan8@gmail.com>
2024-09-04 03:11:26 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FaGithub, FaTwitter } from 'react-icons/fa';
-import { AiFillMediumCircle, AiFillLinkedin } from 'react-icons/ai';
 import { FiMail } from 'react-icons/fi';
 
 
